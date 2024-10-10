f9d04a125fbc8e37126daff4140547bfebb64e5b
fatima <fatimachasi@gmail.com>
2024-10-10 19:57:02 +0200

    Subiendo proyecto

--- src/main/java/com/foodscout/foodscout/servicesImpl/IncidenceServiceImpl.java
+++ src/main/java/com/foodscout/foodscout/servicesImpl/IncidenceServiceImpl.java
@@ -14,6 +14,7 @@ import com.foodscout.foodscout.services.IncidenceService;
 public class IncidenceServiceImpl implements IncidenceService {
 
     @Autowired
+    public
 
     IncidenceRepository incidenceRepository;
 
