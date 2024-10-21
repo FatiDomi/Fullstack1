06c40168c047f4256e4115bf0684e7d8783ca7eb
Fatima Chasi <fatimadominique.s374939@cesurformacion.com>
2024-10-21 14:35:08 +0200

    modificacion appProperties

--- src/main/java/com/foodscout/foodscout/models/Historical.java
+++ src/main/java/com/foodscout/foodscout/models/Historical.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -19,7 +20,9 @@ import jakarta.persistence.Entity;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Historical {
- @GeneratedValue(strategy = GenerationType.AUTO)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(name="createdAt")
     private LocalDateTime createdAt;
